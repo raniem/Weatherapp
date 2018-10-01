91622228a36a61e4eed78c06665dc79ce7b0cdfa
Rami Niemelä <c6raniem@kc408mac03.tamk.ad>
2018-10-01 10:20:09 +0300

    Initial Commit

---
+++ Weatherapp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Weatherapp
+//
+//  Created by Rami Niemelä on 01/10/2018.
+//  Copyright © 2018 Rami Niemelä. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
